df6ccae9206d3bb7ac6f7d899c6d9c0ae8151340
Riku Rouvila <riku.rouvila@gmail.com>
2017-08-26 16:43:05 +0100

    implement subapps with subspace

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
 import UserCreator from "./UserCreator";
 import { connect } from "react-redux";
+import { SubspaceProvider } from "react-redux-subspace";
 
 class App extends Component {
   render() {
     const { users } = this.props;
     return (
       <div>
-        <UserCreator />
-        <UserCreator />
-        <UserCreator />
+        <SubspaceProvider mapState={state => state.app.userCreator1} namespace="userCreator1">
+          <UserCreator />
+        </SubspaceProvider>
+
+        <SubspaceProvider mapState={state => state.app.userCreator2} namespace="userCreator2">
+          <UserCreator />
+        </SubspaceProvider>
+
+        <SubspaceProvider mapState={state => state.app.userCreator3} namespace="userCreator3">
+          <UserCreator />
+        </SubspaceProvider>
+
         <div id="userList">
           {users.map(({ name }) =>
             <div key={name} className="user">
