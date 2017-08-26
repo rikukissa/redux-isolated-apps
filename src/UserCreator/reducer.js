6d4887e4f02735a19b7aa60f9bfb5375e2d2adc8
Riku Rouvila <riku.rouvila@gmail.com>
2017-08-26 15:19:21 +0100

    implement isolation of UserCreator widget with nested stores

--- src/UserCreator/reducer.js
+++ src/UserCreator/reducer.js
@@ -1,13 +1,17 @@
-import { UPDATE_NAME_FIELD } from "./actions";
+import { UPDATE_NAME_FIELD, CREATE_USER } from "./actions";
 
 const initialState = {
-  name: ""
+  name: "",
+  // Only way of getting the information back to the component..
+  createdUser: null
 };
 
 export default function(state = initialState, action) {
   switch (action.type) {
     case UPDATE_NAME_FIELD:
       return { ...state, name: action.payload };
+    case CREATE_USER:
+      return { ...state, createdUser: { name: state.name } };
     default:
       return state;
   }
